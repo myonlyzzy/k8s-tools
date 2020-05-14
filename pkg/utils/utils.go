e2684fc0cc2c3860fea8cb54a5ee52eb843316aa
keliang1 <keliang1@staff.weibo.com>
2020-05-14 15:03:01 +0800

    update pod monitor

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,6 +4,7 @@ import v1 "k8s.io/api/core/v1"
 
 //remove pod from  pod slice
 func RemovePod(p []v1.Pod, i int) []v1.Pod {
-	p[len(p)-1], p[i] = p[i], p[len(p)-1]
-	return p[:len(p)-1]
+	//p[len(p)-1], p[i] = p[i], p[len(p)-1]
+	//return p[:len(p)-1]
+	return append(p[:i], p[i+1:]...)
 }
